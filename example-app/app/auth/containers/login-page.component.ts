3e5e87d4c9ee89eb360b6dd7600b73d147f77bb2
Debasish Kalia <debasish.k@COMAKEIT.COM>
2018-07-06 12:43:13 +0530

    Ngrx implementation

---
+++ example-app/app/auth/containers/login-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Authenticate } from '../models/user';
+import * as fromAuth from '../reducers';
+import * as AuthActions from '../actions/auth.actions';
+
+@Component({
+  selector: 'bc-login-page',
+  template: `
+    <bc-login-form
+      (submitted)="onSubmit($event)"
+      [pending]="pending$ | async"
+      [errorMessage]="error$ | async">
+    </bc-login-form>
+  `,
+  styles: [],
+})
+export class LoginPageComponent implements OnInit {
+  pending$ = this.store.pipe(select(fromAuth.getLoginPagePending));
+  //pending$=this.store.pipe(select(state: State) => state.pending))//select can also accept a function as well.
+  error$ = this.store.pipe(select(fromAuth.getLoginPageError));
+
+  constructor(private store: Store<fromAuth.State>) {}
+
+  ngOnInit() {}
+
+  onSubmit($event: Authenticate) {
+    this.store.dispatch(new AuthActions.Login($event));
+  }
+}
